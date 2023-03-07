5e561f2363a1ff15a35c387902f5d4de0b4938da
Cuong Nguyen <cuong.nguyen@upheads.no>
2023-03-07 14:54:24 +0700

    feat: template method

---
+++ Upheads.DesignPattern.Library/BehavioralPatterns/TemplateMethod/ImportSalesOrder/RawData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upheads.DesignPattern.Library.BehavioralPatterns.TemplateMethod.ImportSalesOrder
+{
+    public class RawData
+    {
+    }
+
+    public class XmlData : RawData
+    {
+    }
+
+    public class PdfData : RawData
+    {
+    }
+}
---
+++ Upheads.DesignPattern.Library/BehavioralPatterns/TemplateMethod/TemplateMethodExecutor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Upheads.DesignPattern.BuildingBlocks;
+using Upheads.DesignPattern.Library.BehavioralPatterns.TemplateMethod.ImportSalesOrder;
+
+namespace Upheads.DesignPattern.Library.BehavioralPatterns.TemplateMethod
+{
+    public class TemplateMethodExecutor : PatternExecutor
+    {
+        public override void Execute()
+        {
+            ConsoleExtension.WriteSeparator("Import sales order example");
+
+            var xmlImporter = new XmlSalesOrderImporter();
+            xmlImporter.Import("/Imports/Xml_SalersOrder.xml");
+
+            Console.WriteLine();
+
+            var pdfImporter = new PdfSalesOrderImporter();
+
+            pdfImporter.Import("/Imports/Pdf_SalersOrder.xml");
+        }
+    }
+}
